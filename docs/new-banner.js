172efd99966a43d5e18d783142d36cbed13a00b8
Brian Cervantes Alvarez <briancervantesalvarez@gmail.com>
2024-08-12 14:01:49 -0700

    Version 3.40

--- docs/new-banner.js
+++ docs/new-banner.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Define arrays of indices for new projects and new blog posts
-    const newProjectsIndices = [0]; // Example indices for new project cards
-    const newBlogPostIndices = [0]; // Example indices for new blog posts
+    const newProjectsIndices = [0, 7]; // Example indices for new project cards [0-3] first row, [4-7] second row
+    const newBlogPostIndices = [0,1]; // Example indices for new blog posts [0-N] where N = n-1 blog pages
 
     // Function to add banners to specified elements
     const addNewBanners = (selector, indices) => {
